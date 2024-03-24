4ae308b95ae5a4d232186c2b467ab91428bc91d7
Preshant Achuthan <prechuthan@users.noreply.github.com>
2024-03-24 05:28:31 +0000

    Added demo of passing vars to views

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Stuff to install
 
 - ejs language extension
 - Fix codespaces ejs support (https://stackoverflow.com/questions/59238175/visual-studio-code-isnt-recognising-ejs)
+- EJS passing of vars (https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application)
--- server.js
+++ server.js
@@ -4,7 +4,19 @@ var app = express()
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/home');
+
+    // Sample vars (to be replaced with DB calls)
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+      ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/home', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
 app.listen(8080);
